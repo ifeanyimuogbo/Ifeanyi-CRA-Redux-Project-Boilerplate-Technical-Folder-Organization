0d8b101873b38417edf03349f55554cb71d8097e
Ifeanyi <doc3coder@gmail.com>
2020-04-01 02:13:07 +0100

    Initial commit of repo

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => <div>
+    <h1>CREATE REACT APP/ REDUX BOILERPLATE - TECHNICAL FOLDER ORGANIZATION</h1>
+    <h6>Created by <a href="https://ifeanyimuogbo.tech">Ifeanyi Muogbo</a></h6>
+</div>
+export default App
